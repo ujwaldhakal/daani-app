18de26881141dd40e223c4a37cb94eb470e9ed69
Ujwal Dhakal <kevin.ujwal@gmail.com>
2020-05-02 23:11:53 +0545

    Product Listing and detail

---
+++ src/components/dashboard/products/single-box.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let name
+  export let slug
+</script>
+<div class="single-product">
+  <a rel=prefetch href="product/{slug}">
+  <img src="http://via.placeholder.com/200x200" alt="{name}">
+  </a>
+  <h1>{name}</h1>
+</div>
---
+++ src/routes/product/[slug].svelte
@@ -0,0 +1,24 @@
+<script>
+  import {stores} from '@sapper/app';
+  import {loadAllPublicProducts} from './../../entity/product'
+
+  const {page} = stores();
+  import {onMount} from 'svelte';
+
+  let slug = $page.params.slug;
+  let filter = {
+    slug: slug
+  }
+  let currentPage = 1;
+
+  let product = {};
+  onMount(async () => {
+    const res = await loadAllPublicProducts(1,filter);
+    if(!res.error) {
+      product = res.data[0];
+    }
+  })
+</script>
+<h1>
+  Product Name  : {product.name}
+</h1>

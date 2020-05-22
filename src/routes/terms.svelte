4d6df35565ff38b6b2012e0ac1524705ff05c131
Ujwal Dhakal <kevin.ujwal@gmail.com>
2020-05-23 01:18:40 +0545

    added terms and condition

--- src/routes/terms.svelte
+++ src/routes/terms.svelte
@@ -5,65 +5,81 @@
   <div class="content">
 
     <div class="header">
-      <h1 class="page-title">Terms and Conditions</h1>
+      <h1 class="page-title">Terms and Conditions for Daani</h1>
+
+      <p>
+        Please read the Terms and Conditions carefully before using Daani.life
+        Welcome to Daani.life a platform where you could donate products freely. By using the Daani, you hereby accept these terms and conditions and represent that you agree to comply with these terms and conditions. If you do not agree to be bound by this User Agreement please do not access, register with, or use this Site.
+
+        Daani reserves the right to change, modify, add, or remove portions of these Terms and Conditions at any time without any prior notification. Changes will be effective when posted on the Site with no other notice provided. Please check these Terms and Conditions regularly for updates. To register an account you could use a Facebook account too and we just use your email address for notification purposes. Note that your email and name will be visible to all as this is a platform to donate your products.
+
+      </p>
     </div>
 
     <div class="container-fluid">
       <div class="row-fluid">
 
-        <h2>Terms of Use</h2>
-        These Terms and Conditions constitute an agreement (“Agreement”) between you (“you”, “your”, “user”, “Customer”)
-        and the Company.Avocado gumbo artichoke ricebean groundnut tigernut. Daikon kakadu plum water spinach garbanzo
-        eggplant fava bean chard rock melon carrot rutabaga water chestnut broccoli courgette onion.
+        <h2>Eligibility / Minimum Age</h2>
 
-        <h2>Eligibility and Identity.</h2>
-        To be eligible to use our Services, you must be at least 13 years old. Sorrel jícama tomato silver beet wattle
-        seed black-eyed pea garlic fennel tigernut okra beetroot shallot. Soko shallot melon dandelion bamboo shoot
-        chickpea soybean pumpkin kakadu plum parsley ricebean grape courgette courgette jícama tatsoi. Black-eyed pea
-        gourd tomatillo arugula cucumber celery mustard black-eyed pea cauliflower soybean rutabaga turnip groundnut.
+        <p>
+          Every age group is allowed to use the application. You shall be responsible to obtain necessary authority to post or retrieve any information and you agree to be accountable for that information.
+        </p>
 
-        <h2>Termination</h2>
-        You may terminate this Agreement at any time by ceasing all use of the Services and by notifying us. The Company
-        may terminate this Agreement, at any time, without notice to you, if it believes, in its sole judgment, that you
-        have breached or may breach any term or condition of this Agreement. Fennel garlic melon broccoli kohlrabi dulse
-        black-eyed pea chicory watercress shallot bamboo shoot cucumber rutabaga ricebean gourd chickweed gumbo. Burdock
-        fennel sorrel cress collard greens tomato tigernut salad chickweed yarrow water spinach catsear earthnut pea
-        cabbage dulse potato. Onion courgette bitterleaf rutabaga tomatillo tigernut groundnut courgette water spinach
-        tomato. Celery ricebean cabbage salsify caulie watercress cress collard greens potato chard gourd pea sprouts
-        cucumber dulse gram. Leek summer purslane tatsoi catsear celtuce broccoli rabe onion zucchini.
+        <h2>Registration</h2>
+        <p>
+          To access certain services offered by our application, we may require that you create an account with us or provide personal information to complete the creation of an account.
+          You are responsible for maintaining the confidentiality of your user identification, password, account details.
+          We will only use your name and email from Facebook as a part of the verification and which will available for the public user to contact you for taking your product.
+          You agree to accept this responsibility and ensure your account and its related details are maintained securely at all times and all necessary steps are taken to prevent misuse of your account.
+        </p>
 
-        <h2>Use of Services & Account</h2>
-        You represent and warrant that you possess the legal right and ability to enter into this Agreement. You agree
-        not to use the Materials, Content, Services, and your Account for any unlawful or abusive purpose or in any way
-        which interferes with our ability to provide Services to our customers, or which damages our property. Chickpea
-        gourd coriander daikon zucchini lettuce tomatillo sierra leone bologi maize parsnip grape melon kohlrabi welsh
-        onion. Celery wakame corn garlic courgette silver beet cabbage gram amaranth jícama bitterleaf. Ricebean bunya
-        nuts prairie turnip water chestnut artichoke cauliflower watercress gourd cabbage okra broccoli rabe. Burdock
-        leek sorrel radicchio azuki bean collard greens winter purslane broccoli rabe gourd water chestnut pumpkin
-        gumbo. Azuki bean green bean kohlrabi kombu aubergine salsify lotus root turnip lentil radicchio nori eggplant
-        sorrel.
+
+        <h2>Understanding of Platform</h2>
+        <p>
+          You agree, understand and acknowledge that this is an online platform that enables you to connect to donated product at any time from any location. You further agree and acknowledge that we are only a facilitator. You agree not to hold Daani responsible for the information posted by these users or for their act in any circumstances however we will constantly moderate the products being added.
+          You further understand that Daani is not involved in the transaction of any goods/services listed in the Site. You will be directly contacting users who have posted advertisements regarding their goods or services offered.
+
+        </p>
+
+
+        <h2>No warranties</h2>
+        <p>
+          Daani expresses no representations or warranties, of any kind related to this application or the materials contained on this application. Also, nothing contained on this Website shall be interpreted as advising you.
+        </p>
+
 
         <h2>Modification to Service</h2>
-        The Company may change, suspend, or discontinue all or any part of the Service at any time, with or without
-        reason. You acknowledge that the operation of the Service may from time to time encounter technical or other
-        problems and may not necessarily continue uninterrupted or without technical or other errors and The Company
-        shall not be responsible to you or others for any such interruptions, errors or problems or an outright
-        discontinuance of the Service.
+        <p>
+          The Company may change, suspend, or discontinue all or any part of the Service at any time, with or without
+          reason. You acknowledge that the operation of the Service may from time to time encounter technical or other
+          problems and may not necessarily continue uninterrupted or without technical or other errors and The Company
+          shall not be responsible to you or others for any such interruptions, errors or problems or an outright
+          discontinuance of the Service.
+        </p>
 
-        <h2>Intellectual Property Ownership</h2>
-        All Materials, Services, Accounts, and content, including, but not limited to, policy information, text,
-        software, music, sound, photographs, video, graphics, the arrangement of text and images, commercially produced
-        information, and other material contained on the Site or through the Services (“Content”), are provided by The
-        Company unless indicated otherwise. Welsh onion tigernut broccoli asparagus brussels sprout jícama eggplant
-        earthnut pea cress chickpea gourd zucchini. Radicchio lentil cucumber groundnut endive kohlrabi winter purslane.
-        Seakale plantain quandong celtuce shallot fennel seakale epazote groundnut dandelion.
+        <h2>Your Conduct</h2>
+        <p>
 
-        <h2>Privacy</h2>
-        Please see our <a href="privacy-policy.html">Privacy Policy</a> as set forth on the site.
+          By posting or submitting any content to the application, including visual files, represent and warrant that
+          You lawfully own or have rights including the copyright, to Your Content
+          None of the materials included in Your Content are unlawful, defamatory, libelous, obscene or pornographic, promotes racism or hatred based on age, gender, race, religion, caste, class
+          Use of Your Content will not violate any legal rights (privacy, intellectual property, etc) of any third party
+          You are solely responsible and accountable for Your Content
 
-        <h2>About These Terms</h2>
-        <p>These Terms and Conditions are just a sample and are not legally binding. Real Terms of Services do not
-          (usually) contain vegetables...</p>
+          Moreover, you agree not to post or list any information:
+          That is related to the sale of any illegal, counterfeit, stolen goods and/or services
+
+          Containing virus, spam, pyramid schemes or any similar forms
+          Related to goods / or services that are prohibited by laws & regulation of Federal Democratic Republic of Nepal
+
+
+        </p>
+
+        <h2>Continued Availability of the site</h2>
+        <p>
+          We will do our utmost to ensure that access to the application is consistently available and is uninterrupted and error-free. However, due to the nature of the Internet and the nature of the Site, this cannot be guaranteed. Additionally, your access to the Site may also be occasionally suspended or restricted to allow for repairs, maintenance, or the introduction of new facilities or services at any time without prior notice. We will attempt to limit the frequency and duration of any such suspension or restriction.
+        </p>
+        
 
       </div>
     </div>
